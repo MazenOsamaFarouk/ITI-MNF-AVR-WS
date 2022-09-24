681859691708e0c0f8be5dcc1137f570b1f6e7bd
MazenOsama <mazen.osama@gmail.com>
2022-09-24 18:31:09 +0200

    added Keypad project

---
+++ 02-Keypad/main.c
@@ -0,0 +1,37 @@
+/*
+ * main.c
+ *
+ *  Created on: Sep 10, 2022
+ *      Author: mazen
+ */
+
+
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+void Keypad_Init(void)
+{
+
+}
+
+u8   Keypad_GetPressedKey(void)
+{
+
+}
+
+
+
+
+
+int main(void)
+{
+
+
+	while(1)
+	{
+
+	}
+}
